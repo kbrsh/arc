0c9313cd21d638ab42373131befeef2ca55a6f38
Kabir Shah <simplecooldude1@gmail.com>
2017-05-29 10:46:58 -0700

    define space clearly

--- drivers/monitor/monitor.h
+++ drivers/monitor/monitor.h
@@ -8,7 +8,7 @@
 #define MAX_COLS 80
 #define SIZE 25 * 80
 
-#define SPACE 0x20 | ((0 << 4) | (15 & 0x0F) << 8)
+#define SPACE 3872
 
 #define SCREEN_CONTROL_PORT 0x3d4
 #define SCREEN_DATA_PORT 0x3d5
