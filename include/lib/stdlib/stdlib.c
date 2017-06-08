ec4db91b65be771a5e9072c076882952ce3fc582
Kabir Shah <simplecooldude1@gmail.com>
2017-06-08 08:14:23 -0700

    add comments

--- include/lib/stdlib/stdlib.c
+++ include/lib/stdlib/stdlib.c
@@ -1,8 +1,10 @@
 #include "../stdlib.h"
 
 u32 intlen(u32 num) {
+  // Length starts at one
   u32 length = 1;
 
+  // Keep dividing by ten (base) to reach length
   while(num > 9) {
     length++;
     num /= 10;
