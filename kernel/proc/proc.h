9646b5d3993adab71bdd9e79e84292f607a1254f
Kabir Shah <simplecooldude1@gmail.com>
2017-06-16 16:07:55 +0530

    basic struct for proc

--- kernel/proc/proc.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROC_H
-#define PROC_H
-
-#include <util/types.h>
-
-#endif
---
+++ kernel/process/process.h
@@ -0,0 +1,10 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include <util/types.h>
+
+struct process {
+	u8 name
+};
+
+#endif
